6bef6f3e69a4db6db6c3ca350ff6d3621d94b149
Aidan Nagorcka-Smith <aidanns@gmail.com>
2017-12-20 23:59:20 +1100

    Added generated Java code from wsdl2java.

---
+++ src/gen/java/com/aidanns/data/wsdl/generated/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://wsdl.data.aidanns.com/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.aidanns.data.wsdl.generated;
